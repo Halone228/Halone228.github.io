d09a23a81f0f02254c53a503dadf881d66f49600
Halone228 <randomnik192@gmail.com>
2022-11-11 01:28:06 +0300

    Fixed page loading

--- src/scripts/get_raw.js
+++ src/scripts/get_raw.js
@@ -1,12 +1,13 @@
 const get_file = (url, el) => {
-    let doc = fetch(url).then(response => response.text())
+console.log('dsadas')
+    fetch(url).then(response => response.text())
     .then(html => {
         let parser = new DOMParser()
-        let doc = parser.parseFromString(html)
-        return doc
+        let doc = parser.parseFromString(html,'text/html')
+        console.log(doc)
+        console.log(el)
+        el.innerHTML = doc.body.innerHTML;
     }).catch(err => {
         console.warn(err)
     })
-    el.innerHTML = doc.querySelector('pre')
-    console.log('dsadas')
-}
+    }
