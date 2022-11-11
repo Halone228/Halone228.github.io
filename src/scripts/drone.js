4e7867b13a4c8eb4a3355a318c0b2ff26046deec
readyyyk <mr.sairexer@gmail.com>
2022-11-12 01:29:39 +0300

    a bit more code, multiple drones

--- src/scripts/drone.js
+++ src/scripts/drone.js
@@ -1,26 +1,54 @@
-let drone = {
+let droneEls = document.querySelectorAll('.drone'),
+drones = [{
     x:0,
     y:0,
     ms: 2000, // in ms
 
-    el: document.querySelector('#drone'),
+    el: droneEls[0]
+},
+{
+    x:0,
+    y:0,
+    ms: 1700, // in ms
+
+    el: droneEls[1]
+},
+{
+    x:0,
+    y:0,
+    ms: 1300, // in ms
+
+    el: droneEls[2]
+},
+{
+    x:0,
+    y:0,
+    ms: 2400, // in ms
+
+    el: droneEls[3]
 }
+]
 
 window.addEventListener( 'load', () => {
 
-    drone.el.style.transitionDuration = `${drone.ms}ms`
+    drones.forEach( drone => {
+// console.log(drone)
 
-    setInterval( () => {
+        drone.el.style.transitionDuration = `${drone.ms}ms`
+        drone.el.style.position = 'absolute'
 
-        let tl = Math.random() * window.innerWidth,
-            tt = Math.random() * window.innerHeight
+        setInterval( () => {
 
-        drone.el.style.transform = drone.x>tl?'rotate(-25deg)':'rotate(25deg)'
-
-        drone.x = tl
-        drone.y = tt
-
-        drone.el.style.left = `${drone.x}px`
-        drone.el.style.top = `${drone.y}px`
-    }, drone.ms )
+            let tl = Math.random() * window.innerWidth,
+                tt = Math.random() * window.innerHeight
+    
+            drone.el.style.transform = drone.x>tl?'rotate(-25deg)':'rotate(25deg)'
+    
+            drone.x = tl
+            drone.y = tt
+    
+            drone.el.style.left = `${drone.x}px`
+            drone.el.style.top = `${drone.y}px`
+        }, drone.ms )
+    } )
 })
--- src/scripts/get_raw.js
+++ src/scripts/get_raw.js
@@ -1,26 +1,33 @@
-const get_file = (url, el) => {
+let getted = {}
 
-    fetch(url).then(response => response.text())
-    .then(html => {
-        let parser = new DOMParser()
-        let doc = parser.parseFromString(html,'text/html')
-        let code = slice===[] ? doc.body.innerHTML : doc.body.innerHTML.split('\n')
-        .slice(slice[0]-1, slice[1]).join('\n');
-        console.log(code)
+const add_to_element = (el, html, slice) => {
+    let parser = new DOMParser(),
+            doc = parser.parseFromString(html,'text/html'),
+            code = slice===[] ? doc.body.innerHTML : doc.body.innerHTML.split('\n').slice(slice[0]-1, slice[1]).join('\n');
+        
         el.innerHTML = `<code>${code}</code>`;
         el.childNodes[0].classList = 'hljs language-python'
         hljs.highlightElement(el.childNodes[0])
-    }).catch(err => {
-        console.warn(err)
-    })
+}
+const get_file = (url, el, slice=[]) => {
+    if(getted[url]!==undefined) {add_to_element(el,getted[url], slice);}
+    else {
+        fetch(url).then(response => response.text())
+        .then(html => {
+            getted[url] = html;
+            add_to_element(el, html,slice);
+        }).catch(err => {
+            console.warn(err)
+        })
+    }
 }
 
 window.onload = () => {
-    document.querySelectorAll('.code-block').forEach( (el) => {
+    document.querySelectorAll('.code-block').forEach( el => {
+        el.classList.add('rounded-3', 'bg-dark')
         let link = el.getAttribute('data-file-link')
         if(el.getAttribute('data-slice-start')){
-            let slice = [el.getAttribute('data-slice-start'),
-             el.getAttribute('data-slice-end')];
+            let slice = [ el.getAttribute('data-slice-start'), el.getAttribute('data-slice-end') ]
             get_file(link,el,slice)
         } else{
             get_file(link, el)
