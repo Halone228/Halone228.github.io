ec002e9bcadfb88d04f3cb16e0d423a8e77e4621
readyyyk <mr.sairexer@gmail.com>
2022-11-11 20:02:10 +0300

    code heghlight done

--- src/scripts/get_raw.js
+++ src/scripts/get_raw.js
@@ -1,19 +1,21 @@
-const get_file = (url, el, slice=[]) => {
+const get_file = (url, el) => {
 
     fetch(url).then(response => response.text())
     .then(html => {
-        let parser = new DOMParser()
-        let doc = parser.parseFromString(html,'text/html')
-        let code = slice===[] ? doc.body.innerHTML : code.split('\n')
-        .slice(slice[0], slice[1]).join('\n');
-        el.innerHTML = `<code fs-codehighlight-element="code">${code}</code>`;
-    }).catch(err => {
+        let code = new DOMParser().parseFromString(html,'text/html').body.innerHTML
+
+        el.innerHTML = `<code>${code}</code>`
+
+        el.childNodes[0].classList = 'hljs language-python'
+        hljs.highlightElement(el.childNodes[0])
+    })
+    .catch(err => {
         console.warn(err)
     })
 }
 
 window.onload = () => {
-    document.querySelectorAll('.code-block').forEach( el => {
+    document.querySelectorAll('.code-block').forEach( (el) => {
         let link = el.getAttribute('data-file-link')
         get_file(link, el)
     } )
